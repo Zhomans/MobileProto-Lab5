b571cf6af603d2bde05f040e63bf1f4b72836187
Chris Lee <sihrc.c.lee@gmail.com>
2013-10-02 21:57:50 -0400

    Added follow button and functionality

--- src/main/java/com/mobileproto/lab5/ProfileActivity.java
+++ src/main/java/com/mobileproto/lab5/ProfileActivity.java
@@ -2,20 +2,27 @@ package com.mobileproto.lab5;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -24,10 +31,15 @@ import java.util.ArrayList;
  */
 public class ProfileActivity extends Activity {
     DBHandler db;
+    public static final String PREFS_NAME = "MyPrefsFile";
+    public static final String PREF_USERNAME = "username";
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        SharedPreferences pref = getSharedPreferences(PREFS_NAME,MODE_PRIVATE);
+        final String primeUser = pref.getString(PREF_USERNAME, null);
 
         Intent intent = getIntent();
 
@@ -49,69 +61,39 @@ public class ProfileActivity extends Activity {
         follow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new AsyncTask<Void, Void, ArrayList<FeedItem>>() {
+                new AsyncTask<Void, Void, Void>() {
                     HttpClient client = new DefaultHttpClient();
                     HttpResponse response;
-                    InputStream inputStream = null;
-                    String result = "";
-
 
                     @Override
                     protected void onPreExecute() {
                         HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000);
                     }
 
-                    protected ArrayList<FeedItem> doInBackground(Void... voids) {
+                    protected Void doInBackground(Void... voids) {
                         ArrayList<FeedItem> feeds = new ArrayList<FeedItem>();
 
                         try {
-                            String website = "http://twitterproto.herokuapp.com/"+ db.getUser() + "/follow";
+                            String website = "http://twitterproto.herokuapp.com/"+ primeUser + "/follow";
                             HttpPost all_tweets = new HttpPost(website);
-                            all_tweets.setHeader("Content-type","application/json");
+                            //all_tweets.setHeader("Content-type","application/json");
+
+                            JSONObject json = new JSONObject();
+                            json.put("username",user);
+                            StringEntity se = new StringEntity(json.toString());
+                            se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE,"application/json"));
+                            all_tweets.setEntity(se);
 
                             response = client.execute(all_tweets);
-                            response.getStatusLine().getStatusCode();
-                            HttpEntity entity = response.getEntity();
-
-                            inputStream = entity.getContent();
-                            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"),8);
-                            StringBuilder sb = new StringBuilder();
-
-                            String line;
-                            String nl = System.getProperty("line.separator");
-                            while ((line = reader.readLine())!= null){
-                                sb.append(line + nl);
-                            }
-                            result = sb.toString();
                         }
-                        catch (Exception e) {e.printStackTrace(); Log.e("Server", "Cannot Establish Connection");
-                            DBHandler db = new DBHandler(getActivity());
-                            db.open();
-                            feeds = db.getSearch(value);
-                        }
-                        finally{
-                            try{if(inputStream != null)inputStream.close();}catch(Exception squish){}}
-
-                        try {
-                            if (!result.equals("")) {
-                                JSONObject jObject = new JSONObject(result);
-                                JSONArray jArray =  jObject.getJSONArray("tweets");
-                                for (int i = 0; i < jArray.length(); i++){
-                                    JSONObject single_feed = jArray.getJSONObject(i);
-                                    FeedItem feeditem = new FeedItem(single_feed.getString("username"),single_feed.getString("tweet"));
-                                    feeds.add(feeditem);}
-                            }
-                        }catch (JSONException e){e.printStackTrace();}
-                        return feeds;
+                        catch (Exception e) {e.printStackTrace(); Log.e("Server", "Cannot Establish Connection");}
+                        return null;
                     }
 
                     protected void onPostExecute(ArrayList<FeedItem> all_feeds){
-                        FeedListAdapter feedListAdapter = new FeedListAdapter(getActivity(), all_feeds);
-                        ListView feedList = (ListView) v.findViewById(R.id.searchResults);
-                        feedList.setAdapter(feedListAdapter);
+                        Toast.makeText(getApplicationContext(),"You are now following " + user,Toast.LENGTH_LONG);
                     }
                 }.execute();
-
             }
          });
     }
